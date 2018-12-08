dace5b4b279d06781f80c1fb11f28df68ef6d3b3
Drago96 <dragproychev@gmail.com>
2018-12-08 22:55:57 +0200

    Data management section completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
+3.times do |topic|
+  Topic.create(title: "Topic #{topic}")
+end
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Random blog body #{blog}"
+    body: "Random blog body #{blog}",
+    topic_id: Topic.last.id
   )
 end
 
@@ -19,5 +24,11 @@ end
     body: "Body #{portfolio_item}",
     main_image: 'https://via.placeholder.com/600x400',
     thumb_image: 'https://via.placeholder.com/350x200'
+  )
+end
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
   )
 end
