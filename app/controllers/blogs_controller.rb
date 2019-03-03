7b2752df0bb0bc875524d819b5de6c7beaba0d51
Drago96 <dragproychev@gmail.com>
2019-03-03 02:42:15 +0200

    Finished up styles for blogs area

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,12 +1,12 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[show edit update destroy toggle_status]
   layout 'blogs'
-  access all: %i[show index], user: { except: %i[destroy new create update edit] }, site_admin: :all
+  access all: %i[show index], user: { except: %i[destroy new create update edit toggle_status] }, site_admin: :all
 
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
   end
 
   # GET /blogs/1
